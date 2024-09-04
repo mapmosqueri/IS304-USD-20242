466642a8c2c6700d5b1e6edc44b929536f503eae
mapmosqueri <162942978+mapmosqueri@users.noreply.github.com>
2024-09-04 09:26:19 -0500

    tareaViernes13
    se elige la tarea del viernes 13

--- SESION04/tarea1.py
+++ SESION04/tarea1.py
@@ -1,2 +1,4 @@
-prueba1
-prueba 2
+/*
+ * Crea una función que sea capaz de detectar si existe un viernes 13 en el mes y el año indicados.
+ * - La función recibirá el mes y el año y retornará verdadero o falso.
+ */
