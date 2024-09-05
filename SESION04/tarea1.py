5f3cf198e9af7ce40ca1b6389ea9095df42cf57f
mapmosqueri <162942978+mapmosqueri@users.noreply.github.com>
2024-09-04 21:36:36 -0500

    Update tarea1.py
    idea principal

--- SESION04/tarea1.py
+++ SESION04/tarea1.py
@@ -2,3 +2,16 @@
  * Crea una función que sea capaz de detectar si existe un viernes 13 en el mes y el año indicados.
  * - La función recibirá el mes y el año y retornará verdadero o falso.
  */
+import datetime
+
+def has_friday_13(month, year):
+    
+    date = datetime.date(year, month, 13)
+    
+    if date.day != 13:
+        return False
+    
+    if date.weekday() == 4:
+        return True
+    
+    return False
